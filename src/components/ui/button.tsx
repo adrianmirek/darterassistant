f908eb2dce300d83ef6b1bf7a63fcce4f6831205
Adrian Mirek <a.mirek@supremo.pl>
2025-12-30 19:14:25 +0100

    fis: mobile, button style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,6 +17,8 @@ const buttonVariants = cva(
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        gradient:
+          "bg-gradient-to-r from-purple-400 to-purple-600 hover:from-purple-500 hover:to-purple-700 text-white transition-all duration-300 shadow-md hover:shadow-lg",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
